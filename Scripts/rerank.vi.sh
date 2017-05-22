6a8a1aa6c242f135d6844fed21760a1f9c03684a
Duong Nguyen Anh Khoa <dnanhkhoa@live.com>
2017-05-22 15:48:35 +0700

    Update

---
+++ Scripts/rerank.vi.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curdir=`pwd`
+rerankdir=$curdir/../Temp/rerank
+rmn=$curdir/../Tools/rmn
+
+cd $rmn
+
+th RM.lua -max_seq_length 80 -min_seq_length 8 -data_dir data/vi -num_layers 1 -mem_size 15 -batch_size 1 -emb_size 128 -rnn_size 128 -nhop 1 -gpuid 0 -init_from data/vi/model.t7 -rerank_dir $rerankdir -rerank_min_seq_length 5
+
+cd $curdir
